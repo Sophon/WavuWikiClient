31ca3fe9802ebd7447927f6eaaffd1feed341980
Le Duy Anh <lenduya@gmail.com>
2025-10-12 03:36:27 +0200

    feat (util): string

--- core/src/commonMain/kotlin/com/example/core/util/String.kt
+++ core/src/commonMain/kotlin/com/example/core/util/String.kt
@@ -3,3 +3,10 @@ package com.example.core.util
 fun String.removeWhiteSpace(): String {
     return this.replace("\\s".toRegex(), "")
 }
+
+fun String.dropFirstAndJoin(delimiter: Char): String {
+    return this
+        .split(delimiter)
+        .drop(1)
+        .joinToString(delimiter.toString())
+}
